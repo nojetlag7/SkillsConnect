2e7a53a6f36654f284fe8c2d325d95665bbdd341
Jeremy O.A Boateng <160321903+nojetlag7@users.noreply.github.com>
2025-10-12 10:33:54 +0000

    able to get gemini working

--- test.js
+++ test.js
@@ -1,25 +1,23 @@
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import {GoogleGenerativeAI as G} from '@google/generative-ai';
 
 dotenv.config();
 
+const genAI = new G(process.env.GEMINI_API_KEY);
 
 
+// TESTING GEMINI API KEY
 
+async function run(){
+	const model =  genAI.getGenerativeModel({model: 'gemini-2.0-flash'});
+	const prompt = 'Write a poem about a lost painter';
 
-const client = new OpenAI({apiKey : process.env.OPENAI_API_KEY});
-// TESTING OPEN AI API KEY
-const openaiApiKey = process.env.OPENAI_API_KEY;
+	const result = await model.generateContent(prompt);
+	const res = await result.response;
+	const text = res.text();
+	console.log(text);
+}
 
-const res = await client.chat.completions.create({
-  model: "gpt-3.5-turbo",
-  messages: [
-	{ role: "user", content: "Say this is a test!" }
-  ],
-});
+run();
 
-console.log(res);
-
-// maybe export if needed later
-module.exports = postmanCollection;
-export default postmanCollection;
